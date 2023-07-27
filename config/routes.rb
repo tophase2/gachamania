75d2c0ae20acfdfc0a68722abdc1176077219a2e
EC2 Default User <ec2-user@ip-172-31-3-38.ap-northeast-1.compute.internal>
2023-07-27 22:05:57 +0000

    [add]ユーザーログイン画面

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,17 @@ Rails.application.routes.draw do
     sessions: 'admin/sessions'
   }
   
+  namespace :admin do
+    root 'homes#top'
+  end
+  
   devise_for :users, skip: [:passwords], controllers: {
     registrations: 'public/registrations',
     sessions: 'public/sessions'
   }
+  
+  scope module: :public do
+    root 'homes#top'
+  end
 
 end
