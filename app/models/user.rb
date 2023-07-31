2d1783b5611982a0fd95dde2d6dbfc31ddb5fc6c
EC2 Default User <ec2-user@ip-172-31-3-38.ap-northeast-1.compute.internal>
2023-07-31 08:06:05 +0000

    [add]投稿機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  has_many :post_images, dependent: :destroy
+         
+  def self.guest
+    find_or_create_by!(name: 'guestuser' ,email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.name = "guestuser"
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,11 @@ Rails.application.routes.draw do
   
   scope module: :public do
     root 'homes#top'
+    resources :post_images, only: [:new, :create, :index, :show]
+  end
+
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
   end
 
 end
