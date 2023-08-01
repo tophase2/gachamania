244a2e1f738bd1de7405554d4f831b2b582e22c1
EC2 Default User <ec2-user@ip-172-31-3-38.ap-northeast-1.compute.internal>
2023-08-01 07:58:26 +0000

    [add]いいね/コメント機能

--- app/controllers/public/post_images_controller.rb
+++ app/controllers/public/post_images_controller.rb
@@ -15,6 +15,14 @@ class Public::PostImagesController < ApplicationController
   end
   
   def show
+    @post_image = PostImage.find(params[:id])
+    @post_comment = PostComment.new
+  end
+  
+  def destroy
+    @post_image = PostImage.find(params[:id])
+    @post_image.destroy
+    redirect_to post_images_path
   end
   
   private
--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,11 @@ Rails.application.routes.draw do
   
   scope module: :public do
     root 'homes#top'
-    resources :post_images, only: [:new, :create, :index, :show]
+    resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+      resource :favorites, only: [:create, :destroy]
+      resources :post_comments, only: [:create, :destroy]
+    end
+    resources :users, only: [:show, :edit, :update]
   end
 
   devise_scope :user do
